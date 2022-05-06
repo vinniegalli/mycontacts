9ef41a2c6bea17b8e32b62e879e7f54d11cc1b1c
Vinícius Galli <vinniegalli@gmail.com>
2022-05-06 00:18:50 -0300

    adicionando pagina principal

---
+++ fe/src/assets/styles/themes/default.js
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#E0E3FF;',
+      light: '#6674F4',
+      main: '#5061fc',
+      darker: '#3346F0',
+    },
+    gray: {
+      900: '#22222',
+      200: '#BCBCBC',
+    },
+  },
+};
